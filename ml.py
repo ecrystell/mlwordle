78fa90874dc4e0954948069b7ca8b306d3caa04c
ecrystell <yeosugar6@gmail.com>
2023-09-13 10:37:30 +0800

    init

---
+++ ml.py
@@ -0,0 +1,39 @@
+import csv
+
+
+with open("ml heroes.csv", "r") as f:
+    csvreader = csv.reader(f)
+    prevrow = []
+    result = []
+    for oldrow in csvreader:
+        row = []
+        for real in oldrow:
+            item = None
+            if "Â" in real:
+                real = real.replace('\xa0', '')
+                real = real.split('Â')
+                item = []
+                for thing in real:
+                  
+                    if thing.isalpha():
+                        item.append(thing)
+            if item:
+                row.append(item)
+            else:
+                row.append(real)
+                
+
+        while '' in row:
+            row.remove('')
+        if len(row) == 1:
+            prevrow += row
+            result.append(prevrow)
+        prevrow = row
+    
+    for row in result:
+        print(row)
+        
+with open("realmlheroes.csv", 'w') as newfile:
+    for row in result:
+        newfile.write(row)
+    
