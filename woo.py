d1cbe02ae05010367258da593216fbdcc66a53b9
ecrystell <yeosugar6@gmail.com>
2023-09-19 23:16:56 +0800

    mlwordle works now

--- woo.py
+++ woo.py
@@ -4,6 +4,7 @@ import sqlite3
 from sqlite3 import Error
 idx = random.randint(1,121)
 
+
 def connect_db(db_path):
     try:
         conn = sqlite3.connect(db_path)
@@ -46,9 +47,56 @@ def read_csv(filepath):
     conn.commit()
     csvfile.close()
 
+
 db_filename = "mlheroes.db"
 conn = connect_db(db_filename)
-create_table(conn)
-read_csv('realmlheroes.csv')
-conn.close()
+#create_table(conn)
+#read_csv('realmlheroes.csv')
 
+
+def generate_hero():
+    heroidx = random.randint(1, 121)
+    hero = conn.execute('''SELECT * FROM heroes WHERE Serial = ?''', (heroidx,)).fetchone()
+    return hero
+
+def all_heroes():
+    return conn.execute('''SELECT Name FROM heroes''').fetchall()
+ans = generate_hero()
+attempt = 0
+
+while True:
+    attempt += 1
+    g = ''
+    checkheroes = all_heroes()
+    #print(checkheroes)
+    while (g,) not in checkheroes and g.lower() != "quit":
+        g = input("Enter hero name: ")
+
+    if g.lower() == 'quit':
+        break
+    
+    guess = conn.execute('''SELECT * FROM heroes WHERE Name = ?''', (g,)).fetchone()
+    corrects = [attempt, None, None, None, "higher", None, None]
+    if guess[1] == ans[1]:
+        print('you win')
+        break
+    for i in range(2, len(guess)):
+        if i == 5:
+            if guess[5] < ans[5]:
+                corrects[4] = "higher"
+            elif guess[5] > ans[5]:
+                corrects[4] = "lower"
+            else:
+                corrects[4] = True
+            continue
+        if guess[i] == ans[i]:
+            corrects[i-1] = True
+        else:
+            corrects[i-1] = False
+    
+    print(';{:<10};{:<25};{:<25};{:<15};{:<10};{:<6};{:<4}'.format(guess[1],guess[2],guess[3],guess[4],guess[5],guess[6],guess[7]))
+    
+    print(';Try: {:<5};{:<25};{:<25};{:<15};{:<10};{:<6};{:<4}'.format(corrects[0],corrects[1],corrects[2],corrects[3],corrects[4],corrects[5],corrects[6]))
+
+    
+conn.close()
